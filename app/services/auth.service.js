4287db9028fb7d52965fef6b530fc1e0da98ab43
taufandwi <taufan.tif@gmail.com>
2020-03-04 10:22:16 +0700

    Update auth.service.js

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'secret';
+const secret = process.env.NODE_ENV === 'production' ? 'secret' : 'secret';
 
 module.exports = {
 	issue: (payload) => jwt.sign(payload, secret, { expiresIn: 10800 }),
